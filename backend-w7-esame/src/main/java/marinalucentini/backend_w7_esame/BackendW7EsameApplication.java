7a07f95555fe4d5b9f6af116c4fe38ee9b207a18
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-07-05 11:11:08 +0200

    Configuration project complete

---
+++ backend-w7-esame/src/main/java/marinalucentini/backend_w7_esame/BackendW7EsameApplication.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w7_esame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendW7EsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendW7EsameApplication.class, args);
+	}
+
+}
---
+++ backend-w7-esame/src/test/java/marinalucentini/backend_w7_esame/BackendW7EsameApplicationTests.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_w7_esame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendW7EsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
